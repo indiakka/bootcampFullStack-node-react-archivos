68907f17329600bc7475bde9a76aabf9df8fe95c
indiakka <78439130+indiakka@users.noreply.github.com>
2022-04-19 16:44:33 +0200

    Convertir el método crear a promesa

--- node-react-archivos/backend/data-handler.js
+++ node-react-archivos/backend/data-handler.js
@@ -2,37 +2,24 @@ const fs = require("fs");
 const path = require("path");
 const directorioBase = path.join(__dirname, "data");
 
-
 const dataHandler = {
-  crear: (
+  crear: async (
     { directorioEntidad = "mascotas", nombreArchivo, datosGuardar },
     callback
   ) => {
-    fs.open(
-      `${directorioBase}/${directorioEntidad}/${nombreArchivo}.json`,
-      "wx",
-      (error, fileDescriptor) => {
-        if (!error && fileDescriptor) {
-          const datosEnString = JSON.stringify(datosGuardar);
-          fs.writeFile(fileDescriptor, datosEnString, (error2) => {
-            if (error2) {
-              return callback(
-                new Error("Error al intentar escribir en el archivo nuevo ")
-              );
-            }
-            fs.close(fileDescriptor, (error3) => {
-              if (error3) {
-                return callback(new Error("Error al cerrar archivo "));
-              }
-              callback(false);
-            });
-          });
-        } else {
-          callback(new Error("No se pudo crear el archivo o ya existe"));
-        }
-      }
-    );
+    try {
+      const fileDescriptor = await fs.promises.open(
+        `${directorioBase}/${directorioEntidad}/${nombreArchivo}.json`,
+        "wx"
+      );
+      const datosEnString = JSON.stringify(datosGuardar);
+      await fs.promises.writeFile(fileDescriptor, datosEnString);
+      return datosGuardar;
+    } catch (error) {
+      return error;
+    }
   },
+
   obtenerUno: async ({
     directorioEntidad = "mascotas",
     nombreArchivo,
@@ -61,7 +48,11 @@ const dataHandler = {
       );
       archivos = archivos.filter((file) => file.includes(".json"));
       const arrayPromesasLeerArchivo = archivos.map((archivo) => {
-        return dataHandler.obtenerUno({directorioEntidad, nombreArchivo: archivo, agregarExtension: false})
+        return dataHandler.obtenerUno({
+          directorioEntidad,
+          nombreArchivo: archivo,
+          agregarExtension: false,
+        });
       });
       let datosArchivos = await Promise.all(arrayPromesasLeerArchivo);
       datosArchivos = datosArchivos.map(JSON.parse);
